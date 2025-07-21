69285a05c02dd8035928249226aa54400b49225a
Pranavkok <pranavkokoff@gmail.com>
2025-07-21 06:33:06 +0530

    create and write models

---
+++ Backend/models/blog.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema({
+    title : {
+        type: String,
+        required: true,
+    },
+    content : {
+        type: String,
+        required: true,
+    },
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+},{
+    timestamps : true 
+})
+
+const BlogModel = mongoose.model('Blog', blogSchema);
+export default BlogModel;
---
+++ Backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    refresh_token: {
+        type : String ,
+        default : ""
+    },
+    last_login_date: {
+        type : Date ,
+        default : ""
+    },
+    blogs: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'Blog',
+    }
+},{
+    timestamps : true 
+})
+
+const UserModel = mongoose.model('User', userSchema);
+export default UserModel;
