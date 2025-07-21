f94868078c8480ce1ad3a2abb92a534dd5a8bc3c
Pranavkok <pranavkokoff@gmail.com>
2025-07-21 06:19:59 +0530

    initial setup done

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+app.use(express.json());
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}));
+app.use(morgan())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Serving from the backend',
+    })
+}
+);
+
+connectDB().then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
